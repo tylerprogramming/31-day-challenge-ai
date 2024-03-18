a2dda69237f11b11abfa48a3753430c02a4507dc
tylerreed <tylerreedytlearning@gmail.com>
2024-03-17 20:41:42 -0400

    added img2text code

---
+++ day_16_image_to_text/main.py
@@ -0,0 +1,16 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+headers = {"Authorization": "Bearer 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"}
+
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+
+output = query("mario.png")
+print(output)
+print(output[0]["generated_text"])
